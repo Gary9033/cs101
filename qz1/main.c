2ef577b19b0d52167447ab8a19376fcb083b289a
Gary9033 <B1029033@cgu.edu.tw>
2022-03-18 23:27:31 +0800

    11

--- qz1/main.c
+++ qz1/main.c
@@ -4,72 +4,87 @@
 #include <string.h>
 
 int main(){
-    FILE* fp_bin;
-    FILE* fp_txt;
-    int count[]={1};
-    if((fp_bin=fopen("count.bin","rb")) != NULL){
-        fread(count, sizeof(count), 1 ,fp_bin);
-        fclose(fp_bin);
+    int a[7]={0},tmp=0,counter,n;
+    char lotto[50]="lotto[0000";
+    char last[50]="0].txt";
+    FILE* fp1;
+    fp1=fopen("counter.bin","r");
+
+    int arr_write[1]={0};
+    
+    if(fp1==NULL){
+     FILE* fptmp= fopen("counter.bin","wb+");
+       fwrite(arr_write,sizeof(int),1,fptmp);
+       fclose(fptmp);
+    }else{
+      fclose(fp1);
     }
-    else{
-        fp_bin=fopen("count.bin","wb+");
-        fwrite(count, sizeof(count), 1, fp_bin);
-        fclose(fp_bin);
-    }
+    int arr_read[1];
+    FILE *fptmp1=fopen("counter.bin","rb");
+       fread(arr_read,sizeof(int),1,fptmp1);
+       counter=arr_read[0];
+       fclose(fptmp1);
+    last[0]=counter+49;
+    strcat(lotto,last);   
+    ++counter;
+    FILE *fp;
+    fp=fopen(lotto,"w+");
+    printf("歡迎光臨長庚樂透彩購買機台\n");
+    printf("請問你要買幾組樂透彩: ");
+    scanf("%d",&n);
+    printf("已為您購買的%d組樂透組合輸出至 lotto.txt\n",n);
+    fprintf(fp,"======== lotto649 =========\n");
+    fprintf(fp,"=======+ No.%05d +========\n",counter);
     time_t curtime;
     time(&curtime);
-    int arr_num[5][7],group,flag,temp,ID;
-    srand((unsigned) time(NULL));
-    printf("歡迎光臨長庚樂透彩購買機台,請問您要買幾組樂透彩:");
-    scanf("%d",&group);
 
-    char filename[50];
-    snprintf(filename, sizeof(filename), "lotto[%04d].txt",count[0]);
-    fp_txt = fopen(filename,"w+");
-    fprintf(fp_txt,"========= lotto649 =========\n");
-    fprintf(fp_txt,"========+ No.%05d +========\n",count[0]);
-    fprintf(fp_txt,"= %.*s =\n", 24, ctime(&curtime));
-    for(int i=0; i<5; i++){
-        if(group>=(i+1)){
-            for(int j=0; j<=6;){
-                do{
-                    j!=6? (arr_num[i][j] = rand()%69+1) : (arr_num[i][j] = rand()%10+1);
-                    flag=0;
-                    for(int k=0; k<j; k++){
-                        temp=0;
-                        if(arr_num[i][j]==arr_num[i][k]){
-                            flag=1;
-                        }
-                        else if(j<5 && arr_num[i][j]<arr_num[i][k]){
-                            temp=arr_num[i][j];
-                            arr_num[i][j]=arr_num[i][k];
-                            arr_num[i][k]=temp;
-                        }
-                    }
-                }while(flag==1);
-                j++;
+    fprintf(fp,"= %.24s=\n",ctime(&curtime));
+
+    for (int i=0;i<5;i++) {
+        fprintf(fp,"[%d]: ",i+1);
+        if (i<=(n-1)) {
+            for (int i=0;i<6;i++) {
+            a[i]=rand()%69+1;
             }
+            a[6]=rand()%9+1;
+            for (int i=0;i<6;i++) {
+              for (int j=i+1;j<6;j++) {
+                if (a[i]==a[j]) {
+                a[j]=rand()%69+1;
+                }
+             }
+           }
+        for (int j=5;j>0;j--) {
+        for (int k=0;k<=j-1;k++) {
+            if (a[k]>a[k+1]) {
+                tmp=a[k];
+                a[k]=a[k+1];
+                a[k+1]=tmp;
+            }
+          }
         }
-    }
-
-    for(int i=0; i<5; i++){
-        fprintf(fp_txt,"[%d] : ",i+1);
-        for(int j=0; j<=6; j++){
-            if(group>=(i+1)){
-                fprintf(fp_txt,"%02d ",arr_num[i][j]);
-            }
-            else{
-                fprintf(fp_txt,"-- ");
-            }
+        for (int i=0;i<7;i++) {
+            if (a[i]<10) {
+                fprintf(fp,"0%d ",a[i]);
+            } else {fprintf(fp,"%d ",a[i]);
+          }
+         }
+        fprintf(fp,"\n");
+        } else {
+        for (int i=0;i<7;i++) {
+            fprintf(fp,"-- ");
         }
-        fprintf(fp_txt,"\n");
-    }
-    fprintf(fp_txt,"========= csie@CGU =========\n");
-    printf("已為您購買的%d組樂透組合輸出至%s\n",group,filename);
-    fclose(fp_txt);
-
-    count[0]++;
-    fp_bin=fopen("count.bin","wb+");
-    fwrite(count,sizeof(count),1,fp_bin);
-    fclose(fp_bin);
-}
+              fprintf(fp,"\n");
+        }     tmp=0;
+              for (int i=0;i<7;i++) {
+                  a[i]=0;
+                }
+              
+        } fprintf(fp,"======== csie@CGU =========\n");
+        fclose(fp);
+        
+    arr_write[0]=counter;
+    FILE* fp2=fopen("counter.bin","wb");
+    fwrite(arr_write,sizeof(int),1,fp2);
+    fclose(fp2);
+}   
