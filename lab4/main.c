994f8f7c922d25e2d4e730890d4ca6b2300f260a
Gary9033 <91184169+Gary9033@users.noreply.github.com>
2022-06-15 16:52:46 +0800

    Update main.c

--- lab4/main.c
+++ lab4/main.c
@@ -2,42 +2,42 @@
 int find_n(int a[10],int j){
    for(int i=0;i<10;i++){
         if(a[i]==j){
-            return i+1;
+            return i;
         }
     }
 }
 
 void j(){
-    int n[10]={1,2,3,4,5,6,7,8,9,0};
+    int n[10]={1,2,5,4,3,6,7,8,9,0};
     int num;
     int *p;
     scanf("%d",&num);
     int i=find_n(n,num);
-    p=n+i-1;
+    p=n+i;
 
     printf("&n[%d]->%p, n[%d]=%d;p->%p,*p=%d\n",i,n+i-1,i,num,p,*p);
-
 }
 void k(){
-    int n[10]={1,2,3,4,5,6,7,8,9,0};
+    int n[10]={6,4,7,2,0,9,8,1,5,3};
     int *ap[10];
-    int tmp=0;
-    for(int i = 9; i > 0; i--){
-    for(int j = 0; j <= i-1; j++){
-        if( *(n+j) > *(n+j+1)){
-            tmp = *(n+j);
-            *(n+j) = *(n+j+1);
-            *(n+j+1) = tmp;
-          }
+    int m[10]={0};
+    for(int i=0;i<10;i++){
+        m[i]=n[i];
+    }
+    int tmp;
+    for(int j=10;j>0;j--){
+        for(int i=0;i<j;i++){
+            if(m[i+1]<m[i]){
+              tmp =m[i+1];
+              m[i+1]=m[i];
+              m[i]=tmp;
+            }
         }
     }
     for(int i=0;i<10;i++){
-            ap[i]=n+i;
-            printf("&n[%d]->%p, n[%d] = %d,ap[%d]->%p, *ap[%d] = %d\n ", i, ap[i], i, *(ap[i]),i, ap[i], i, *(ap[i]));
-
+        ap[i]=n+find_n(n,m[i]);
+        printf("&n[%d]->%p, n[%d]=%d ;ap[%d]->%p,*ap[%d]=%d\n",find_n(n,i),n+find_n(n,i),find_n(n,i),i,i,ap[i],i,*ap[i]);
     }
-
-
 
 }
  int main(){
